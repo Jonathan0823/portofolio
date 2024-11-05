4c853e993eb9994090a4f109dd3b22e706550097
Guinn <eguinjonathan08@gmail.com>
2024-11-05 17:56:47 +0700

    Fix Hero component layout and background styling for improved responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,15 +15,12 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
       <div>
-        <div className="h-screen w-fullbg-[#010523]  bg-grid-white/[0.3] relative flex items-center justify-center">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#010523]  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="h-screen w-full bg-[#010523]  bg-grid-white/[0.3] relative flex items-center justify-center">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#010523] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
 
-          <div className="flex justify-center">
-            <div className=" max-w-[89vw]">
-              <h2 className="text-white">TEST</h2>
-            </div>
+          <div className="flex justify-center relative my-20 z-10">
+            <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw items-center justify-center]"></div>
           </div>
-
         </div>
       </div>
     </div>
