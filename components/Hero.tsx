24f10b27e3d9fb7911f700dacacbb5d768704737
Guinn <eguinjonathan08@gmail.com>
2024-11-05 18:15:44 +0700

    Add TextGenerateEffect component and enhance Hero layout with new styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const Hero = () => {
   return (
@@ -14,13 +15,19 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div>
-        <div className="h-screen w-full bg-[#010523]  bg-grid-white/[0.3] relative flex items-center justify-center">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#010523] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
 
-          <div className="flex justify-center relative my-20 z-10">
-            <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw items-center justify-center]"></div>
-          </div>
+      <div className="h-screen w-full bg-[#010523]  bg-grid-white/[0.05] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#010523] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Test
+          </h2>
+
+          <TextGenerateEffect className="text-4xl" words="Web Developer sdfsdf dfsdf sdfsdf"></TextGenerateEffect>
+
         </div>
       </div>
     </div>
