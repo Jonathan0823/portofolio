3593b88796ca71616d46baddc05a8e1476187ace
Guinn <eguinjonathan08@gmail.com>
2024-11-05 17:37:36 +0700

    Add ThemeProvider for dark mode support and enhance Hero component with new background utilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+      <ThemeProvider defaultTheme="dark" enableSystem={false}>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
