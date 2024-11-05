e3079557f4d3db2fbff1561016145967fd5f7885
Guinn <eguinjonathan08@gmail.com>
2024-11-05 16:44:50 +0700

    Enhance Home component styling; update layout with Tailwind CSS classes and add a greeting header. Update package manager and add overrides in package.json. Extend color palette in Tailwind configuration.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="text-white">Hello World</h1>
       </div>
     </main>
   );
