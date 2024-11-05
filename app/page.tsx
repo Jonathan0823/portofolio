a41a6333e19baebbd6e13e837547a42354e38388
Guinn <eguinjonathan08@gmail.com>
2024-11-05 19:44:06 +0700

    Add FloatingNav component to Home page for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
-    <main className="relative bg-[#010523] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-[#010523] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 h-screen">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[{ name: "Home", link: "/" }]} />
         <Hero />
       </div>
     </main>
