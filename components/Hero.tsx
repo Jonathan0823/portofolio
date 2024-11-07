2166434bb5bfae06c1babeacfef4ee215befbc4b
Guinn <eguinjonathan08@gmail.com>
2024-11-07 12:36:57 +0700

    Update Hero component padding for improved layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="pb-20 h-screen">
+    <div className="pb-44 ">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
